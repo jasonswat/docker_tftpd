9f76219351b17b4c024c93217b9bcee64830c7c6
Swatniki, Jason <Jason.Swatniki@nordstrom.com>
2019-02-18 11:52:08 -0800

    Updated startup script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-/usr/sbin/lighttpd start 
-/usr/sbin/in.tftpd --foreground --user tftp --secure /tftpboot
+
+/usr/sbin/lighttpd start
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to lighttpd: $status"
+  exit $status
+fi
+
+/usr/sbin/in.tftpd --user tftp --secure /tftpboot
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start tftpd: $status"
+  exit $status
+fi
+
+while sleep 60; do
+  ps aux |grep lighttpd |grep -q -v grep
+  LIGHTTPD=$?
+  ps aux |grep in.tftpd |grep -q -v grep
+  TFTPD=$?
+  if [ $LIGHTTPD -ne 0 -o $TFTPD -ne 0 ]; then
+    echo "One of the processes has exited."
+    exit 1
+  fi
+done
